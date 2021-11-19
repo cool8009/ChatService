f4ce3ede10c463a0bfaa04cb4b84d6204c9ae619
cool8009 <cool80092@gmail.com>
2021-11-19 05:28:17 +0200

    commitfirst

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackgammonChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        private readonly IDictionary<string, UserConnection> _connections;
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            _botUser = "MyChat";
+            _connections = connections;
+        }
+
+        //override from hub and disconnect connection
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+                Clients.Group(userConnection.Room)
+                    .SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+                SendConnectedUsers(userConnection.Room);
+            }
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+        //get details about user, the message context,room and send
+        public async Task SendMessage  (string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room)
+                 .SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
+
+        //get details about user connection, the  room, enter the room
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            //to notify connection to the room
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            _connections[Context.ConnectionId] = userConnection;
+
+            
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
+                $"{userConnection.User} joined {userConnection.Room}");
+
+            await SendConnectedUsers(userConnection.Room);
+        }
+
+        //get all connected users by room
+        public Task SendConnectedUsers(string room)
+        {
+            var users = _connections.Values
+                .Where(c => c.Room == room)
+                .Select(c => c.User);
+
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
+        }
+        
+    }
+}
